35eb1f8bdf1c1909b48806cabe07b01491d7b87e
Cardinelli <nugokvachadze@gmail.com>
2019-08-16 15:43:03 +0400

    Add route and implements api key

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+//LOAD env
+dotenv.config({path: './.env'});
+
+const app = express();
+
+// Dev logging
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+
+// Profile routes
+
+app.use('/api/v1/profile', require('./routes/profile'));
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Server runnning in ${process.env.NODE_ENV} mode on port ${port}`);
+});
+
